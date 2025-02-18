c441ccb92753a4f8fee0fabefbb97d5f340fd4a6
MacBook M1 Pro <prettyfly@korea.com>
2025-02-18 17:33:39 +0900

    hashids 데모 추가

--- README.md
+++ README.md
@@ -2,3 +2,39 @@
 
 > https://blog.logrocket.com/how-to-set-up-node-typescript-express/
 
+## Sqids(구 Hashids)
+
+> https://sqids.org/
+
+### 유형별 최대값(sqids를 8자리로 고정)
+
+```text
+# 알파뱃 대소문자, 숫자(총 62개 문자)
+# alphabet : vPQ0MdX1YSu7nkU5WJDzRwGEqFxcZAjB3KgNoiTV6lm294bCIpOthyHa8sLref
+3,142,742,836,020 => pppppppp
+
+# 알파뱃 대소문자, 숫자(총 54개 문자 : 혼동이 될 수 있는 문자 i, l, o, I, L, O, 0, 1 제외)
+# alphabet : WBgKVACxUeHamzRYvZrwfSET29hqJM78nFPtd5pc3G6DQsykjNuX4b
+1,174,711,139,836 => XXXXXXXX
+
+# 알파벳 대문자, 수장(총 36개 문자)
+# alphabet : PNZX081GJHC725TSLFEU96KIQRWB4DYVOM3A
+64,339,296,874 => 55555555
+```
+
+### 고려사항
+
+```text
+시퀀스 숫자를 베이스로 하여 인코딩을 한다.
+보안성이 필요한 정보에는 사용하지 않는다.
+alphabet 값을 shuffling 하여 값을 유추하기 어렵도록 할 수 있다.
+인코딩한 값에 동일한 문자열이 반복될 수도 있다.
+```
+
+## Hashids
+
+```text
+sqids와 달리 salt 값을 지정할 수 있음.
+salt값과 alphabet 값을 shuffling하여 값을 유축하기 어렵게 할 수 있다.
+salt값에 의해 hashids의 길이가 달라지지는 않는다.
+```
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import Sqids from 'sqids'
+import Hashids from 'hashids'
 
 dotenv.config();
 
@@ -17,20 +18,35 @@ app.get("/sqids", (req: Request, res: Response) => {
     alphabet: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
     // alphabet: 'vPQ0MdX1YSu7nkU5WJDzRwGEqFxcZAjB3KgNoiTV6lm294bCIpOthyHa8sLref',
     // alphabet: 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789',
-    // alphabet: 'x8faJ3rYzkQ6FRywX4eZEpVSqhjtDnT7C9AP5sbWNcgvKMdGuHUB2m',
+    // alphabet: 'WBgKVACxUeHamzRYvZrwfSET29hqJM78nFPtd5pc3G6DQsykjNuX4b',
     // alphabet: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890',
     // alphabet: 'PNZX081GJHC725TSLFEU96KIQRWB4DYVOM3A',
     minLength: 8,
   })
   // const id = sqids.encode([12345678, 43668760])// "B4aajs"
-  // alphanuberic                              : 2999999999999 => xvjxVGza
-  // alphanumeric(i, l, o, I, L, O, 0, 1 제외) : 1174711139836 => vcSgMbqEU
-  // capatal and numeric                       : 59999999999 => 7FQUHJTC
-  const id = sqids.encode([2999999999999])// "B4aajs"
+  // alphanuberic                                 : 3142742836020 => pppppppp
+  // alphanumeric(remove i, l, o, I, L, O, 0, 1)  : 1174711139836 => XXXXXXXX
+  // capatal and numeric                          : 64339296874 => 55555555
+  const id = sqids.encode([64339296874])// "B4aajs"
   const numbers = sqids.decode(id) // [1, 2, 3]
   res.send(id + ' => ' + numbers + '\n');
 });
 
+app.get("/hashids", (req: Request, res: Response) => {
+  const hashids = new Hashids(
+    /* salt: */ 'This is my salt',
+    /* minLength: */ 8,
+    /* alphabet: '🌸💮🏵️🌹🥀🌺🌻🌼🌷🌱🌲🌳🌴🌵🌾🌿☘️🍀🍁🍂🍃🍄', */
+    /* alphabet: */ 'WBgKVACxUeHamzRYvZrwfSET29hqJM78nFPtd5pc3G6DQsykjNuX4b',
+    /* these chars can't be next to one another: */ '',
+  );
+
+  const orgValue = 6433929687;
+  const value = hashids.encode([orgValue]);
+  const decoded = hashids.decode(value);
+  res.send(decoded + ' => ' + value + '\n');
+});
+  
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
