cb446b6b3eb76d3cf43e3c261d4420ccb4d7cedc
MacBook M1 Pro <prettyfly@korea.com>
2025-02-18 18:38:48 +0900

    가변정보를 환경변수로 분리

--- src/index.ts
+++ src/index.ts
@@ -14,36 +14,37 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.get("/sqids", (req: Request, res: Response) => {
+  const alphabet = process.env.SQIDS_ALPHABET || '';
+  const minLength = parseInt(process.env.SQIDS_MIN_LENGTH || '');
+
   const sqids = new Sqids({
-    alphabet: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
-    // alphabet: 'vPQ0MdX1YSu7nkU5WJDzRwGEqFxcZAjB3KgNoiTV6lm294bCIpOthyHa8sLref',
-    // alphabet: 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789',
-    // alphabet: 'WBgKVACxUeHamzRYvZrwfSET29hqJM78nFPtd5pc3G6DQsykjNuX4b',
-    // alphabet: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890',
-    // alphabet: 'PNZX081GJHC725TSLFEU96KIQRWB4DYVOM3A',
-    minLength: 8,
+    alphabet: alphabet,
+    minLength: minLength,
   })
-  // const id = sqids.encode([12345678, 43668760])// "B4aajs"
   // alphanuberic                                 : 3142742836020 => pppppppp
   // alphanumeric(remove i, l, o, I, L, O, 0, 1)  : 1174711139836 => XXXXXXXX
   // capatal and numeric                          : 64339296874 => 55555555
   const id = sqids.encode([64339296874])// "B4aajs"
   const numbers = sqids.decode(id) // [1, 2, 3]
+  console.log(`id: ${id}, numbers: ${numbers}`);
   res.send(id + ' => ' + numbers + '\n');
 });
 
 app.get("/hashids", (req: Request, res: Response) => {
+  const salt = process.env.HASHIDS_SALT || '';
+  const alphabet = process.env.HASHIDS_ALPHABET || '';
+  const minLength = parseInt(process.env.HASHIDS_MIN_LENGTH || ''); 
   const hashids = new Hashids(
-    /* salt: */ 'This is my salt',
-    /* minLength: */ 8,
-    /* alphabet: '🌸💮🏵️🌹🥀🌺🌻🌼🌷🌱🌲🌳🌴🌵🌾🌿☘️🍀🍁🍂🍃🍄', */
-    /* alphabet: */ 'WBgKVACxUeHamzRYvZrwfSET29hqJM78nFPtd5pc3G6DQsykjNuX4b',
+    /* salt: */ salt,
+    /* minLength: */ minLength,
+    /* alphabet: */ alphabet,
     /* these chars can't be next to one another: */ '',
   );
 
   const orgValue = 6433929687;
   const value = hashids.encode([orgValue]);
   const decoded = hashids.decode(value);
+  console.log(`id: ${value}, numbers: ${decoded}`);
   res.send(decoded + ' => ' + value + '\n');
 });
   
